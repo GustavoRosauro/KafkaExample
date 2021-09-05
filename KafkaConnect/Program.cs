06c59a89fa7e27f32b0574f9999080d328ba7a2b
GustavoRosauro <gustavosrosauro@gmail.com>
2021-09-05 14:57:12 -0300

    Comunicacao Finalizada

---
+++ KafkaConnect/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace KafkaConnect
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092"                
+            };
+            using (var producer = new ProducerBuilder<Null, string>(config).Build())
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    await producer.ProduceAsync("quickstart-events", new Message<Null, string> { Value = $"Gustavo {i}" });
+                }
+            }
+        }
+    }
+}
---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,34 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace KafkaConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConsumerConfig
+            {
+                BootstrapServers = "localhost:9092",
+                GroupId = "quickstart-events"
+            };
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe("quickstart-events");
+                CancellationTokenSource cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+                while (true)
+                {
+                    var cr = consumer.Consume(cts.Token);
+                    Console.WriteLine($"Consumed message '{cr.Message.Value}' at '{cr.TopicPartitionOffset}'");
+                }
+            }
+        }
+    }
+}
